c7c6612e145714f682f27ced79400215b4c72b84
fiach <fiach.reid@gmail.com>
2018-03-02 10:02:16 +0000

    Changes before go-live

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ var http = require('http');
 var url = require('url');
 const webpush = require('web-push');
 http.createServer(function (req, res) {
-    var queryData = url.parse(req.url, true).query;
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var vapidKeys = {
-        publicKey: queryData.publicKey,
-        privateKey: queryData.privateKey
-    };
-    webpush.setVapidDetails(
-      'mailto:fiach.reid@gmail.com',
-      vapidKeys.publicKey,
-      vapidKeys.privateKey
-    );
-    var pushSubscription = {
-      "endpoint": queryData.endpoint,
-      "expirationTime":null,
-      "keys":{
-          "p256dh": queryData.p256dh,
-          "auth": queryData.auth
-        }
-    };
-    webpush.sendNotification(pushSubscription, queryData.payload);
-}).listen(8081);
+	try
+	{
+		var queryData = url.parse(req.url, true).query;
+		res.writeHead(200, {'Content-Type': 'text/html'});		
+		webpush.setVapidDetails(
+		  'mailto:fiach.reid@gmail.com',
+		  queryData.publicKey,
+		  queryData.privateKey
+		);
+		var pushSubscription = {
+		  "endpoint": queryData.endpoint,
+		  "expirationTime":null,
+		  "keys":{
+			  "p256dh": queryData.p256dh,
+			  "auth": queryData.auth
+			}
+		};
+		webpush.sendNotification(pushSubscription, queryData.payload);
+		res.end("OK");
+	}
+	catch (ex)
+	{
+		res.end(ex.toString());
+	}   
+}).listen(process.env.PORT);
